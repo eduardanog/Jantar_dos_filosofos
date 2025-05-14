73a220722c16190043fa44c6c2f37da3d08bacce
Maria Eduarda <eduardanogfreitas@gmail.com>
2025-05-14 20:26:42 -0300

    alteração na main

--- main.py
+++ main.py
@@ -3,22 +3,20 @@ from Jantar import JantarDosFilosofos
 from Interface import InterfaceJantar
 
 def main():
-    # Configuração do jantar
+    #configuração do jantar
     num_filosofos = 5
     jantar = JantarDosFilosofos(num_filosofos)
     
-    # Configuração da interface
+    #configuração da interface
     interface = InterfaceJantar(jantar)
     jantar.registrar_observador(interface)
     
-    # Criação dos filósofos
+    #criação dos filósofos
     filosofos = [Filosofo(i, jantar) for i in range(num_filosofos)]
     
-    # Inicia os filósofos
     for f in filosofos:
         f.start()
     
-    # Inicia a interface
     interface.executar()
 
 if __name__ == "__main__":
